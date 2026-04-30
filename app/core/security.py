321363ad5fbf2d3295f74d2ed002568e1854c160
editook <edito.ok123@gmail.com>
2026-04-30 18:21:18 -0400

    changes clean passwarod

--- app/core/security.py
+++ app/core/security.py
@@ -7,7 +7,8 @@ from app.core.config import settings
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    clean_password = plain_password.strip()[:72]
+    return pwd_context.verify(clean_password, hashed_password)
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
